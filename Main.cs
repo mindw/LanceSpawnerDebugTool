43e716eedd22ad6390db7253a0d3837131ff2c70
Michael P. Starkweather <mpstark@gmail.com>
2019-06-08 18:04:37 -0500

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using Harmony;
+using HBS.Logging;
+
+namespace LanceSpawnerDebugTool
+{
+    public static class Main
+    {
+        internal static ILog HBSLog;
+        internal static ModSettings Settings;
+
+        public static void Init(string modDir, string settings)
+        {
+            var harmony = HarmonyInstance.Create("io.github.mpstark.LanceSpawnerDebugTool");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            HBSLog = Logger.GetLogger("LanceSpawnerDebugTool");
+            Settings = ModSettings.Parse(settings);
+        }
+    }
+}
---
+++ Patches/TagSetQueryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using BattleTech.Data;
+using Harmony;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
+namespace LanceSpawnerDebugTool.Patches
+{
+    [HarmonyPatch(typeof(TagSetQueryExtensions), "GetMatchingUnitDefs")]
+    public static class GetMatchingUnitDefsPatch
+    {
+        public static void Postfix(List<UnitDef_MDD> __result)
+        {
+            Main.HBSLog.Log("GetMatchingUnitDefs returns:");
+            foreach (var unitDefMdd in __result)
+            {
+                Main.HBSLog.Log($"  {unitDefMdd.UnitDefID}");
+            }
+        }
+    }
+}
